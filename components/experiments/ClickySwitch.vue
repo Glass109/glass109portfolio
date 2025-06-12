d2295a484582889820f5024954221e823737a5f4
Raul Donde <glas109@gmail.com>
2025-06-12 01:42:32 -0600

    feat: add experiments page with Clicky Switch component and styling
    - Introduced a new page at /experiments with a collection of interactive experiments.
    - Added ClickySwitch component to the experiments section.
    - Styled the page with a unique halftone background and crystal container design.
    - Updated package.json to include jsfxr dependency for sound effects.

---
+++ components/experiments/ClickySwitch.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { defineModel, onMounted } from 'vue';
+
+const checked = defineModel({
+    type: Boolean,
+    default: false,
+});
+
+</script>
+
+<template>
+    <div>
+        <label class="switch">
+            <input type="checkbox" v-model="checked"s>
+            <span class="slider"></span>
+        </label>
+    </div>
+</template>
+
+<style scoped>
+.switch {
+    position: relative;
+    display: inline-block;
+    width: 50px;
+    height: 22px;
+}
+
+.switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+.slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    transition: .4s cubic-bezier(0, 1, 0, 1);
+    border-radius: 20px;
+}
+
+.slider:before {
+    position: absolute;
+    content: "";
+    height: 18px;
+    width: 18px;
+    left: 3px;
+    bottom: 2px;
+    background-color: white;
+    transition: .4s cubic-bezier(0, 1, 0, 1);
+    border-radius: 50%;
+}
+
+input:checked+.slider {
+    background-color: #2196F3;
+}
+
+input:checked+.slider:before {
+    transform: translateX(26px);
+}
+</style>
---
+++ pages/experiments/index.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+import ClickySwitch from '@/components/experiments/ClickySwitch.vue';
+interface ExperimentSection {
+  id: number;
+  title: string;
+  description: string;
+  component: Component;
+}
+const experimentSections: ExperimentSection[] = [
+  { id: 1, title: 'Clicky Switch', description: 'Un switch muy bonito y satisfactorio de encender', component: ClickySwitch },
+];
+</script>
+
+<template>
+  <main class="halftone min-h-screen">
+    <div class="container mx-auto">
+      <header class="title crystalcontainer">
+        <p>Colección de experimentos y otras fregaderas que me gusta programar</p>
+      </header>
+
+      <section class="experiment-grid">
+        <article v-for="experiment in experimentSections" :key="experiment.id" class="crystalcontainer">
+          <header>
+            <h2 class="font-bold text-2xl">{{ experiment.title }}</h2>
+          </header>
+          <div>
+            <component :is="experiment.component" />
+          </div>
+        </article>
+      </section>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+  margin-bottom: 2rem;
+  font-size: 2rem;
+  border-radius: 0.5rem;
+  font-weight: bold;
+}
+
+.halftone {
+  background-image:
+    radial-gradient(circle at center, whitesmoke 0.25rem, transparent 0),
+    radial-gradient(circle at center, azure 0.25rem, transparent 0);
+  background-size: 1.3rem 1.3rem;
+  background-position: 0 0, 0.65rem 0.65rem;
+}
+
+.crystalcontainer {
+    background: linear-gradient(to bottom, #ffffff50, #f0f0f050, #ffffff50);
+    border: 1px solid #ccc;
+    padding: 1rem;
+    box-shadow:
+    4px 4px 2px rgba(0, 0, 0, 0.1),
+    inset 4px 4px 4px rgba(255, 255, 255, 1);
+  text-shadow: white 0 0 4px;
+
+}
+</style>
